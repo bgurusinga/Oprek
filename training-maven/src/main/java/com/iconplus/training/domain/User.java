a5cb072784846304df79ce897a900659cb5ca92b
bgurusinga <b.frypan@gmail.com>
2012-12-10 15:21:57 +0700

    maven masuk

---
+++ training-maven/src/main/java/com/iconplus/training/domain/User.java
@@ -0,0 +1,12 @@
+package com.iconplus.training.domain;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: BayuFrioGS
+ * Date: 10/12/12
+ * Time: 15:02
+ * To change this template use File | Settings | File Templates.
+ */
+public class User {
+
+}
